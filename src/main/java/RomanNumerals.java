24fcedab4bc1f3106432ea15e54e274f67b68d97
craw-codurance <cameron.raw@codurance.com>
2021-02-23 15:20:42 +0100

    Reviews Sandros solution and redid entire kata following example

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,27 +1,38 @@
 public class RomanNumerals {
-    public String convert(int i) {
+
+    public String convert(int decimal) {
         StringBuilder romanNumerals = new StringBuilder();
-        if(i <= 3){
-            for(int x = 0; x < i; x++){
-                romanNumerals.append("I");
-            }
+        for(RomanToDecimal romanToDecimal : RomanToDecimal.values()){
+           while(decimal >= romanToDecimal.decimal){
+               decimal -= romanToDecimal.decimal;
+               romanNumerals.append(romanToDecimal.roman);
+           }
         }
-        if(i == 4){
-            romanNumerals.append("IV");
-        }
-        if(i >= 5 && i < 9){
-            romanNumerals.append("V");
-            i -= 5;
-            for(int x = 0; x < i; x++){
-                romanNumerals.append("I");
-            }
-        }
-        if(i == 9){
-            romanNumerals.append("IX");
-        }
-        if(i == 10){
-            romanNumerals.append("X");
-        }
+        romanNumerals.append("I".repeat(Math.max(0, decimal)));
         return romanNumerals.toString();
+    }
+
+    enum RomanToDecimal {
+        THOUSAND("M", 1000),
+        NINE_HUNDRED("CM", 900),
+        FIVE_HUNDRED("D", 500),
+        FOUR_HUNDRED("CD", 400),
+        HUNDRED("C", 100),
+        NINETY("XC", 90),
+        FIFTY("L", 50),
+        FORTY("XL", 40),
+        TEN("X", 10),
+        NINE("IX", 9),
+        FIVE("V", 5),
+        FOUR("IV", 4),
+        ONE("I",1);
+
+        private final String roman;
+        private final int decimal;
+
+        RomanToDecimal(String roman, int decimal){
+            this.roman = roman;
+            this.decimal = decimal;
+        }
     }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,56 +1,65 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsTest {
+    RomanNumerals romanNumerals;
+    @BeforeEach
+    void Setup(){
+       this.romanNumerals = new RomanNumerals();
+    }
     @Test
-    void ReturnsIWhenPassed1(){
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void Returns_I_When_Input_Is_1(){
         assertEquals("I", romanNumerals.convert(1));
     }
     @Test
-    void ReturnsIIWhenPassed2(){
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void Returns_II_When_Input_Is_2(){
         assertEquals("II", romanNumerals.convert(2));
     }
     @Test
-    void ReturnsIIIWhenPassed3(){
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void Returns_III_When_Input_Is_3(){
         assertEquals("III", romanNumerals.convert(3));
     }
     @Test
-    void ReturnsIVWhenPassed4(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("IV", romanNumerals.convert(4));
-    }
-    @Test
-    void ReturnsVWhenPassed5(){
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void Returns_V_When_Input_Is_5(){
         assertEquals("V", romanNumerals.convert(5));
     }
     @Test
-    void ReturnsVIWhenPassed6(){
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void Returns_VI_When_Input_Is_6(){
         assertEquals("VI", romanNumerals.convert(6));
     }
     @Test
-    void ReturnsVIIWhenPassed7(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("VII", romanNumerals.convert(7 ));
+    void Returns_VII_When_Input_Is_7(){
+        assertEquals("VII", romanNumerals.convert(7));
     }
     @Test
-    void ReturnsVIIIWhenPassed8(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("VIII", romanNumerals.convert(8 ));
+    void Returns_VIII_When_Input_Is_8(){
+        assertEquals("VIII", romanNumerals.convert(8));
     }
     @Test
-    void ReturnsIXWhenPassed9(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("IX", romanNumerals.convert(9 ));
+    void Returns_X_When_Input_Is_10(){
+        assertEquals("X", romanNumerals.convert(10));
     }
     @Test
-    void ReturnsXWhenPassed10(){
-        RomanNumerals romanNumerals = new RomanNumerals();
-        assertEquals("X", romanNumerals.convert(10 ));
+    void Returns_XV_When_Input_Is_15(){
+        assertEquals("XV", romanNumerals.convert(15));
     }
+    @Test
+    void Returns_XXV_When_Input_Is_25(){
+        assertEquals("XXV", romanNumerals.convert(25));
+    }
+    @Test
+    void Returns_IV_When_Input_Is_4(){
+        assertEquals("IV", romanNumerals.convert(4));
+    }
+    @Test
+    void Returns_IX_When_Input_Is_9(){
+        assertEquals("IX", romanNumerals.convert(9));
+    }
+    @Test
+    void Returns_MMCMXLVII_When_Input_Is_2947(){
+        assertEquals("MMCMXLVII", romanNumerals.convert(2947));
+    }
+
 }
